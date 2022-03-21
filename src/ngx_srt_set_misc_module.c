f25a4b2905210e8ed3f154f962e78d72297aa66f
shamama <yair.shamama@kaltura.com>
2022-03-21 12:38:02 +0200

    put the jey and the iv in the same directive of the decrypt.

--- src/ngx_srt_set_misc_module.c
+++ src/ngx_srt_set_misc_module.c
@@ -9,11 +9,6 @@
 
 
 typedef struct {
-    ngx_str_t  key;
-    ngx_str_t  iv;
-} ngx_srt_utils_conf_t;
-
-typedef struct {
     ngx_srt_complex_value_t  value;
 } ngx_srt_base64_ctx_t;
 
@@ -29,39 +24,24 @@ enum {
 };
 
 
-static void *ngx_srt_utils_create_conf(ngx_conf_t *cf);
-static char *ngx_srt_utils_decrypt(ngx_conf_t *cf, ngx_command_t *cmd,
+static char *ngx_srt_set_misc_decrypt(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
-static char *ngx_srt_utils_base64(ngx_conf_t *cf, ngx_command_t *cmd,
+static char *ngx_srt_set_misc_base64(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
 
 
-static ngx_command_t  ngx_srt_utils_commands[] = {
-
-    { ngx_string("encrypt_key"),
-      NGX_SRT_MAIN_CONF|NGX_CONF_TAKE1,
-      ngx_conf_set_str_slot,
-      NGX_SRT_MAIN_CONF_OFFSET,
-      offsetof(ngx_srt_utils_conf_t, key),
-      NULL },
-
-    { ngx_string("encrypt_iv"),
-      NGX_SRT_MAIN_CONF|NGX_CONF_TAKE1,
-      ngx_conf_set_str_slot,
-      NGX_SRT_MAIN_CONF_OFFSET,
-      offsetof(ngx_srt_utils_conf_t, iv),
-      NULL },
+static ngx_command_t  ngx_srt_set_misc_commands[] = {
 
     { ngx_string("set_decode_base64"),
       NGX_SRT_MAIN_CONF|NGX_CONF_TAKE2,
-      ngx_srt_utils_base64,
+      ngx_srt_set_misc_base64,
       NGX_SRT_MAIN_CONF_OFFSET,
       0,
       NULL },
 
     { ngx_string("set_aes_decrypt"),
-      NGX_SRT_MAIN_CONF|NGX_CONF_TAKE2,
-      ngx_srt_utils_decrypt,
+      NGX_SRT_MAIN_CONF|NGX_CONF_TAKE4,
+      ngx_srt_set_misc_decrypt,
       NGX_SRT_MAIN_CONF_OFFSET,
       0,
       NULL },
@@ -69,21 +49,21 @@ static ngx_command_t  ngx_srt_utils_commands[] = {
       ngx_null_command
 };
 
-static ngx_srt_module_t  ngx_srt_utils_module_ctx = {
+static ngx_srt_module_t  ngx_srt_set_misc_module_ctx = {
     NULL,                                  /* preconfiguration */
     NULL,                                  /* postconfiguration */
 
-    ngx_srt_utils_create_conf,             /* create main configuration */
+    NULL,                                  /* create main configuration */
     NULL,                                  /* init main configuration */
 
     NULL,                                  /* create server configuration */
     NULL                                   /* merge server configuration */
 };
 
-ngx_module_t  ngx_srt_utils_module = {
+ngx_module_t  ngx_srt_set_misc_module = {
     NGX_MODULE_V1,
-    &ngx_srt_utils_module_ctx,             /* module context */
-    ngx_srt_utils_commands,                /* module directives */
+    &ngx_srt_set_misc_module_ctx,             /* module context */
+    ngx_srt_set_misc_commands,                /* module directives */
     NGX_SRT_MODULE,                        /* module type */
     NULL,                                  /* init master */
     NULL,                                  /* init module */
@@ -96,26 +76,8 @@ ngx_module_t  ngx_srt_utils_module = {
 };
 
 
-static void *
-ngx_srt_utils_create_conf(ngx_conf_t *cf)
-{
-    ngx_srt_utils_conf_t  *ucf;
-
-    ucf = ngx_palloc(cf->pool, sizeof(ngx_srt_utils_conf_t));
-    if (ucf == NULL) {
-        return NULL;
-    }
-
-    ucf->key.len = 0;
-    ucf->key.data = NULL;
-    ucf->iv.len = 0;
-    ucf->iv.data = NULL;
-
-    return ucf;
-}
-
 static ngx_int_t
-ngx_srt_utils_base64_variable(ngx_srt_session_t *s, ngx_srt_variable_value_t *v,
+ngx_srt_set_misc_base64_variable(ngx_srt_session_t *s, ngx_srt_variable_value_t *v,
     uintptr_t data)
 {
     ngx_str_t              val, decode_str;
@@ -128,7 +90,7 @@ ngx_srt_utils_base64_variable(ngx_srt_session_t *s, ngx_srt_variable_value_t *v,
 
     if (ngx_srt_complex_value(s, &base64->value, &val) != NGX_OK) {
         ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
-        "ngx_srt_utils_base64_variable: failed to eval complex value");
+        "ngx_srt_set_misc_base64_variable: failed to eval complex value");
         return NGX_ERROR;
     }
 
@@ -148,7 +110,7 @@ ngx_srt_utils_base64_variable(ngx_srt_session_t *s, ngx_srt_variable_value_t *v,
 }
 
 static char *
-ngx_srt_utils_base64(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+ngx_srt_set_misc_base64(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
     ngx_str_t                       *value, name;
     ngx_srt_variable_t              *var;
@@ -188,7 +150,7 @@ ngx_srt_utils_base64(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
         return NGX_CONF_ERROR;
     }
 
-    var->get_handler = ngx_srt_utils_base64_variable;
+    var->get_handler = ngx_srt_set_misc_base64_variable;
     var->data = (uintptr_t) base64;
 
     return NGX_CONF_OK;
@@ -196,7 +158,7 @@ ngx_srt_utils_base64(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 
 
 ngx_int_t
-ngx_srt_utils_decrypt_aes(ngx_connection_t *c, ngx_str_t key,
+ngx_srt_set_misc_decrypt_aes(ngx_connection_t *c, ngx_str_t key,
     ngx_str_t iv, ngx_str_t input, ngx_str_t *dst)
 {
     size_t             block_size, buf_size;
@@ -204,7 +166,7 @@ ngx_srt_utils_decrypt_aes(ngx_connection_t *c, ngx_str_t key,
     const EVP_CIPHER  *cipher;
 
     if (key.len != ngx_encrypt_key_length || iv.len < MD5_DIGEST_LENGTH) {
-        ngx_log_error(NGX_LOG_ERR, c->log, 0, "ngx_srt_utils_decrypt_aes:"
+        ngx_log_error(NGX_LOG_ERR, c->log, 0, "ngx_srt_set_misc_decrypt_aes:"
             "key length and iv length is not correct");
         return NGX_ERROR;
     }
@@ -217,7 +179,7 @@ ngx_srt_utils_decrypt_aes(ngx_connection_t *c, ngx_str_t key,
     dst->len = buf_size;
     dst->data = ngx_palloc(c->pool, buf_size);
     if (dst->data == NULL) {
-        ngx_log_error(NGX_LOG_ERR, c->log, 0, "ngx_srt_utils_decrypt_aes:"
+        ngx_log_error(NGX_LOG_ERR, c->log, 0, "ngx_srt_set_misc_decrypt_aes:"
             "alloc failed");
         return NGX_ERROR;
     }
@@ -226,7 +188,7 @@ ngx_srt_utils_decrypt_aes(ngx_connection_t *c, ngx_str_t key,
 
     if (!EVP_DecryptInit_ex(ctx, cipher, NULL, key.data, iv.data))
     {
-       ngx_log_error(NGX_LOG_ERR, c->log, 0, "ngx_srt_utils_decrypt_aes:"
+       ngx_log_error(NGX_LOG_ERR, c->log, 0, "ngx_srt_set_misc_decrypt_aes:"
             "EVP_DecryptInit_ex failed");
         EVP_CIPHER_CTX_cleanup(ctx);
         return NGX_ERROR;
@@ -234,7 +196,7 @@ ngx_srt_utils_decrypt_aes(ngx_connection_t *c, ngx_str_t key,
 
     if (!EVP_DecryptUpdate(ctx, dst->data, (int *) &dst->len, input.data, input.len))
     {
-       ngx_log_error(NGX_LOG_ERR, c->log, 0, "ngx_srt_utils_decrypt_aes:"
+       ngx_log_error(NGX_LOG_ERR, c->log, 0, "ngx_srt_set_misc_decrypt_aes:"
             "EVP_DecryptUpdate failed");
         EVP_CIPHER_CTX_cleanup(ctx);
         return NGX_ERROR;
@@ -242,7 +204,7 @@ ngx_srt_utils_decrypt_aes(ngx_connection_t *c, ngx_str_t key,
 
     if (!EVP_DecryptFinal_ex(ctx, dst->data + dst->len, (int *) &dst->len))
     {
-        ngx_log_error(NGX_LOG_ERR, c->log, 0, "ngx_srt_utils_decrypt_aes:"
+        ngx_log_error(NGX_LOG_ERR, c->log, 0, "ngx_srt_set_misc_decrypt_aes:"
             "EVP_DecryptFinal_ex failed");
         EVP_CIPHER_CTX_cleanup(ctx);
         return NGX_ERROR;
@@ -254,7 +216,7 @@ ngx_srt_utils_decrypt_aes(ngx_connection_t *c, ngx_str_t key,
 }
 
 static ngx_int_t
-ngx_srt_utils_decrypt_variable(ngx_srt_session_t *s,
+ngx_srt_set_misc_decrypt_variable(ngx_srt_session_t *s,
     ngx_srt_variable_value_t *v, uintptr_t data)
 {
     ngx_str_t               val, decrypt_str;
@@ -267,14 +229,14 @@ ngx_srt_utils_decrypt_variable(ngx_srt_session_t *s,
 
     if (ngx_srt_complex_value(s, &decrypt->value, &val) != NGX_OK) {
         ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
-        "ngx_srt_utils_decrypt_variable: failed to eval complex value");
+        "ngx_srt_set_misc_decrypt_variable: failed to eval complex value");
         return NGX_ERROR;
     }
 
     decrypt_str.len = val.len;
     decrypt_str.data = val.data;
 
-    ngx_srt_utils_decrypt_aes(s->connection, decrypt->key, decrypt->iv,
+    ngx_srt_set_misc_decrypt_aes(s->connection, decrypt->key, decrypt->iv,
         val, &decrypt_str);
 
     v->valid = 1;
@@ -290,41 +252,34 @@ ngx_srt_utils_decrypt_variable(ngx_srt_session_t *s,
 }
 
 static char *
-ngx_srt_utils_decrypt(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+ngx_srt_set_misc_decrypt(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
-    ngx_srt_utils_conf_t  *ucf = conf;
-
-    ngx_str_t                       *value, name;
-    ngx_srt_variable_t              *var;
-    ngx_srt_encrypt_decrypt_ctx_t           *decrypt;
-    ngx_srt_compile_complex_value_t  ccv;
-
-    if (ucf->key.len == 0 || ucf->iv.len == 0) {
-        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "key and iv must be defined");
-        return NGX_CONF_ERROR;
-    }
+    ngx_str_t                        *value, name;
+    ngx_srt_variable_t               *var;
+    ngx_srt_encrypt_decrypt_ctx_t    *decrypt;
+    ngx_srt_compile_complex_value_t   ccv;
 
     decrypt = ngx_pcalloc(cf->pool, sizeof(ngx_srt_encrypt_decrypt_ctx_t));
     if (decrypt == NULL) {
         return NGX_CONF_ERROR;
     }
 
-    decrypt->key = ucf->key;
-    decrypt->iv  = ucf->iv;
-
     value = cf->args->elts;
+
+    decrypt->key = value[1];
+    decrypt->iv  = value[2];
 
     ngx_memzero(&ccv, sizeof(ngx_srt_compile_complex_value_t));
 
     ccv.cf = cf;
-    ccv.value = &value[1];
+    ccv.value = &value[3];
     ccv.complex_value = &decrypt->value;
 
     if (ngx_srt_compile_complex_value(&ccv) != NGX_OK) {
         return NGX_CONF_ERROR;
     }
 
-    name = value[2];
+    name = value[4];
 
     if (name.data[0] != '$') {
         ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
@@ -340,7 +295,7 @@ ngx_srt_utils_decrypt(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
         return NGX_CONF_ERROR;
     }
 
-    var->get_handler = ngx_srt_utils_decrypt_variable;
+    var->get_handler = ngx_srt_set_misc_decrypt_variable;
     var->data = (uintptr_t) decrypt;
 
     return NGX_CONF_OK;
